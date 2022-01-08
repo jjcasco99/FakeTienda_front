389402282f2e332f87f0f5c74cb88151d194514a
jjcasco99 <juanjocasco28@gmail.com>
2022-01-08 11:47:00 +0100

    añadida la cara trasera de los juegos

--- src/components/Home/Products/Products.jsx
+++ src/components/Home/Products/Products.jsx
@@ -4,11 +4,19 @@ import React from "react";
 const Products = (props) => {
 
   return <div className="juego">
-          <p>{props.info.nombre}</p>
-          <img src={props.info.imagen} alt={props.info.nombre}/>
-          <p>Valoración: {props.info.relevancia}</p>
-          <p>Precio: {props.info.precio}€</p>
-          <p>Empresa: {props.info.empresa}</p>
+            <div className="cara-frontal"> 
+              <p>{props.info.nombre}</p>
+              <img src={props.info.imagen} alt={props.info.nombre}/>
+              <p>Valoración: {props.info.relevancia}</p>
+              <p>Precio: {props.info.precio}€</p>
+              <p>Empresa: {props.info.empresa}</p>
+            </div>
+            <div className="cara-trasera">
+              <p>Datos del fabricante:</p>
+              <p>Nombre empresa: {props.info.empresa}</p>
+              <p>CIP: {props.info.cip}</p>
+              <p>Dirección: {props.info.direccion}</p>
+            </div>
          </div>;
 };
 
